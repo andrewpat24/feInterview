7aafbc2f6cc520d99f5ed8327ecba631e2fcb4c7
Andrew Patterson <andrew.patterson@essention.co>
2018-07-31 09:25:00 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var objCollection = [
+    {
+        img: 'http://getbootstrap.com/docs/4.1/assets/brand/bootstrap-social.png',
+        title: 'Test Card',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Integer vel placerat magna. Suspendisse potenti. Sed hendrerit gravida libero, 
+        eu convallis lectus auctor eget. Maecenas non massa vulputate, suscipit velit vel,
+        sagittis leo. Donec mollis rhoncus eros. 
+        Curabitur vel ex eget risus tincidunt accumsan. 
+        Suspendisse eget dapibus ligula, non consequat dui.`,
+        url: 'https://www.google.com'
+    }
+]
